4d0f20f51da75ce1bb58c25feea885e5dfe17e3e
Lisa <wamaithak05@gmail.com>
2024-04-01 21:27:08 +0300

    index.js updated

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ fetch('http://localhost:3000/characters')
 
 .then(data  => {
     console.log(data);
-    //Create characters via DOM Manipulation
+    //Create characters via DOM to be shown on browser
     data.forEach(candidate =>{
         const characterList = document.createElement('li')
         characterList.textContent = candidate.name;
@@ -35,7 +35,42 @@ fetch('http://localhost:3000/characters')
             const votesForm = document.getElementById('votes-form')
             const votes = document.getElementById('votes')
 
-            
+            //submit votes
+            votesForm.addEventListener('submit', (votes))
+            votes.preventDefault()// resets to default
+            let newVote = parseInt(votes.ariaValueMax, 10)
+            currentVOte = currentVote + newVote;
+            characterVotes.textContent = currentVote;
+
         })
+
     })
+
+    //append child
+    characters.appendChild(characterList)
+})
+
+//shows content to the user when invoked
+document.addEventListener('DOMContentLoaded', characterData)
+
+//workng on adding votes
+const inputVotes = document.querySelector("input#votes");
+const animalVotes = document.querySelector("span#vote-count");
+const form = document.querySelector("form#votes-form");
+form.addEventLidtener("submit", (e) => {
+    e.preventDefault();
+    console.log(currentAnimal);
+    currentAnimal.votes += parseInt(e.target.votes.value)
+    showAnimal(currentAnimal);
+    form.reset ();
+});
+
+//working on the reset button
+const resetVotes = document.querySelector("button#reset-btn");
+resetVotes.computedStyleMap.cursor ="pointer";
+resetVotes.addEventListener("click", () => {
+    currentAnimal.votes = 0;
+    showAnimal.votes = 0;
+    showAnimal(currentAnimal);
+    form.reset();
 })
